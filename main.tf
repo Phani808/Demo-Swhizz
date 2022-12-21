c1344e81f9bdef85ef920314dc82989db4c71593
Phani808 <112239169+Phani808@users.noreply.github.com>
2022-12-21 13:27:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -100,8 +100,8 @@ resource "aws_instance" "awsvm" {
 
 locals {
   environment                = "test"
-  availability_zones         = ["us-east-2a", "us-east-2b", "us-east-2c"]
-  region                     = "us-east-2"
+  availability_zones         = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  region                     = "us-east-1"
   vpc_cidr                   = "10.0.0.0/16"
   instance_count             = 1
   ami                        = "ami-02701bcdc5509e57b"
